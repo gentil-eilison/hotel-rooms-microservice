d7015132623ea7d0e53e3b1261f1fb7e1de167a0
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2024-04-05 14:35:30 -0300

    feature(bookings): adds proto_class and proto_list_class

--- hotel_rooms/bookings/api/serializers.py
+++ hotel_rooms/bookings/api/serializers.py
@@ -1,9 +1,15 @@
 from django_socio_grpc import proto_serializers
 
 from hotel_rooms.bookings.models import Booking
+from ..grpc.bookings_pb2 import (
+    BookingResponse,
+    BookingListResponse,
+)
 
 
 class BookingProtoSerializer(proto_serializers.ModelProtoSerializer):
     class Meta:
         model = Booking
         fields = ("id", "from_date", "until_date", "price", "room", "payment_id", "user_id")
+        proto_class = BookingResponse
+        proto_list_class = BookingListResponse
