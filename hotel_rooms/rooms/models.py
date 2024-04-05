9dcba05aa6b8e57827ce1091c9b443195799f8d6
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2024-04-05 18:37:11 -0300

    fix(rooms): adds MinValueValidator to numeric fields in order to bypass Proto3 default numeric field value

--- hotel_rooms/rooms/models.py
+++ hotel_rooms/rooms/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Room(models.Model):
     available = models.BooleanField(default=False, verbose_name="Available")
-    n_of_beds = models.PositiveSmallIntegerField(verbose_name="Number of beds")
-    numeration = models.PositiveSmallIntegerField(verbose_name="Room numeration")
-    n_of_bathrooms = models.PositiveSmallIntegerField(verbose_name="Number of bathrooms")
+    n_of_beds = models.PositiveSmallIntegerField(verbose_name="Number of beds", validators=[MinValueValidator(1)])
+    numeration = models.PositiveSmallIntegerField(verbose_name="Room numeration", validators=[MinValueValidator(1)])
+    n_of_bathrooms = models.PositiveSmallIntegerField(verbose_name="Number of bathrooms", validators=[MinValueValidator(1)])
 
     def __str__(self):
         return f"Room number {self.numeration}"
